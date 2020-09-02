fa80e0e067393debc8e603170e187676071fcdb2
Anmol Dhiman <imanmoldh121@gmail.com>
2020-09-02 18:11:19 +0530

    Updates

---
+++ precache-manifest.efca89039e38cbe1a51f62e04f5b9ca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5227f054e59994f5239b7cd581efd75",
+    "url": "/cd_project/index.html"
+  },
+  {
+    "revision": "137d823bf3f9102b9875",
+    "url": "/cd_project/static/css/main.9956be1e.chunk.css"
+  },
+  {
+    "revision": "cb3bfdbf7f267e5c8838",
+    "url": "/cd_project/static/js/2.1e2c8095.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cd_project/static/js/2.1e2c8095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137d823bf3f9102b9875",
+    "url": "/cd_project/static/js/main.f34682da.chunk.js"
+  },
+  {
+    "revision": "b1e75a9397eb3a13b1b9",
+    "url": "/cd_project/static/js/runtime-main.43cf1ee4.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/cd_project/static/media/college_02.619ee97e.jpg"
+  }
+]);
